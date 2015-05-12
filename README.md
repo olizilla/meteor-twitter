469b379d4670cec016994deb03e9c16477799bf9
olizilla <oli@zilla.org.uk>
2015-05-12 16:32:59 +0100

    Strip it back

--- README.md
+++ README.md
@@ -1,15 +1,11 @@
-# cscottnet:twitter
+# tableflip:twitter
 
-This is a Meteor binding for the npm
-[`twitter`](https://www.npmjs.org/package/twitter) package.
+This is a Meteor binding for the npm [`twitter`](https://www.npmjs.org/package/twitter) package.
 
-## Installation
-```
-meteor add cscottnet:twitter
-```
+Forked from https://github.com/cscott/meteor-twitter which was way outta date, and I didn't get on with the wrapping style.
 
-## License
 
+
+## Original license
 Copyright (c) 2014 C. Scott Ananian
-
 Licensed under GPLv2.
--- package.js
+++ package.js
@@ -1,24 +1,15 @@
 Package.describe({
   name: 'cscottnet:twitter',
   summary: 'Interface with Twitter APIs on Meteor server',
-  version: '0.2.13',
+  version: '1.2.5',
   git: 'https://github.com/cscott/meteor-twitter.git'
 });
 
-Npm.depends({"twitter": "0.2.13"});
+Npm.depends({"twitter": "1.2.5"});
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@0.9.0');
   api.addFiles([
     'twitter.js'
   ], 'server');
   if (api.export) { api.export('Twitter', 'server'); }
-});
-
-Package.onTest(function(api) {
-  /*
-  api.use('tinytest');
-  api.use('cscottnet:twitter');
-  api.addFiles('twitter-tests.js');
-  */
 });
